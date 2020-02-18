a074d0d75a6e846dd6e8a73a50dd6df9cc9aa6ee
DexterNeuf <35289316+DexterNeuf@users.noreply.github.com>
2020-02-17 22:49:04 -0800

    added json file and json file reader

--- bot.js
+++ bot.js
@@ -1,8 +1,13 @@
-console.log("hello");
+// console.log("hello");
+"use strict";
+var JSONItems = [];
+$.getJSON("graveheart.json", function(data) {
+  JSONItems = data;
+  console.log(JSONItems.songs[0].lyrics);
+});
+// const Twit = require("twit");
 
-const Twit = require('twit');
+// const config = require("./config");
+// const T = new Twit(config);
 
-const config = require('./config');
-const T = new Twit(config);
-
-T.post('statuses/update', { status: 'hello world!' })
+// T.post("statuses/update", { status: "hello world!!" });
