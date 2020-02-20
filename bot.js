d732bdddb1ad92c874a4debcac74cdabb9c37ba7
DexterNeuf <dxneufeld14@gmail.com>
2020-02-20 13:08:48 -0800

    added functionality for create a string of a line of the song got from the json data by inputing which line of the song you want

--- bot.js
+++ bot.js
@@ -1,14 +1,23 @@
-// console.log("hello");
 "use strict";
-var JSONItems = [];
-$.getJSON("graveheart.json", function(data) {
-  JSONItems = data;
-  console.log(JSONItems.songs[0].lyrics);
-  //https://stackoverflow.com/questions/14480345/how-to-get-the-nth-occurrence-in-a-string
-});
+const fs = require('fs');
+let rawData = fs.readFileSync('graveheart.json');
+let parseData = JSON.parse(rawData);
+let x = parseData.songs[0].lyrics
+let count = 0
+let position = x.indexOf('\n')
+while (position !== -1) {
+    count++
+    position = x.indexOf('\n', position + 1);
+    if (count === 7) {
+        break;
+    }
+}
+// console.log(x.indexOf('\n', position + 1));
+
+console.log(x.substring(position, x.indexOf('\n', position + 1)));
 // const Twit = require("twit");
 
 // const config = require("./config");
 // const T = new Twit(config);
 
-// T.post("statuses/update", { status: "hello world!!" });
+// T.post("statuses/update", { status: "hello w0rld!!" });
