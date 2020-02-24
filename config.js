b69eaf2937525fffbb1036af77552b894fc0804b
Dex <dxneufeld14@gmail.com>
2020-02-24 12:40:52 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    consumer_key: 'KebdqIZysrVN2wENvLxEg1qyO',
-    consumer_secret: 'yDLv7Ie6c6j1VFCRiugQQWBEEGgiuGKvWmWz9HUrGVq6JH8i96',
-    access_token: '1227730157558829056-d2d1DLn8ZMHjq4ba5RtEBeNbsgk4Ng',
-    access_token_secret: 'yCQ2C3BGiASPktDPic7KaApRigcOmrHbcCtc8ze1v0qd0'
+    consumer_key: 'enter your key',
+    consumer_secret: 'enter your key',
+    access_token: 'enter your token',
+    access_token_secret: 'enter your token'
     // timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
     // strictSSL: true,     // optional - requires SSL certificates to be valid.
 }
